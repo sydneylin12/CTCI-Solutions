17717b7e64396bfb54af89974484ee5fbfd47b25
Sid Lin <sydney.lin12@gmail.com>
2020-04-13 11:24:58 -0500

    Problem 10 in python

---
+++ Python/scheduler.py
@@ -0,0 +1,22 @@
+import time
+
+# Call a funciton f after n milliseconds
+def schedule(f, n):
+    # Sleep for n milliseconds
+    time.sleep(n / 1000)
+    return f()
+
+def fac(n):
+    if n == 0:
+        return 1
+    else:
+        return n * fac(n-1)
+
+def foo():
+    print("Called from foo!")
+
+def main():
+    schedule(foo, 3000)
+
+if __name__ == "__main__":
+    main()
